c45e859b3245f0e43526232ed2f321f0121cf6ff
admoisesdev <moi.prado@duocuc.cl>
2025-04-15 14:00:32 -0400

    feat: Crear store para almacenar y limpiar imagenes subidas desde la app movil

--- config/adapters/__test__/storage.adapter.test.ts
+++ config/adapters/__test__/storage.adapter.test.ts
@@ -1,6 +1,8 @@
+import { Alert } from "react-native";
 import { StorageAdapter } from "../storage.adapter";
 
 import AsyncStorage from "@react-native-async-storage/async-storage";
+jest.spyOn(Alert, "alert");
 
 
 describe("Probar adaptador StorageAdapter", () => {
@@ -12,6 +14,7 @@ describe("Probar adaptador StorageAdapter", () => {
   const mockValue = "testValue";
 
   const mockKeys = ["key1", "key2", "key3"];
+  const mockError = new Error("Test error");
 
   test("Debe almacenar un elemento en AsyncStorage", async () => {
     await StorageAdapter.setItem(mockKey, mockValue);
@@ -54,4 +57,67 @@ describe("Probar adaptador StorageAdapter", () => {
 
     expect(AsyncStorage.clear).toHaveBeenCalled();
   });
+
+  test("Debe manejar errores al almacenar un elemento en AsyncStorage", async () => {
+    (AsyncStorage.setItem as jest.Mock).mockRejectedValue(mockError);
+
+    await StorageAdapter.setItem(mockKey, mockValue);
+
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith(mockKey, mockValue);
+    expect(Alert.alert).toHaveBeenCalledWith("Error", "Failed to save data");
+  });
+
+  test("Debe manejar errores al obtener un elemento de AsyncStorage", async () => {
+    (AsyncStorage.getItem as jest.Mock).mockRejectedValue(mockError);
+
+    const result = await StorageAdapter.getItem(mockKey);
+
+    expect(AsyncStorage.getItem).toHaveBeenCalledWith(mockKey);
+    expect(Alert.alert).toHaveBeenCalledWith("Error", "Failed to get data");
+    expect(result).toBeNull();
+  });
+
+  test("Debe manejar errores al eliminar un elemento de AsyncStorage", async () => {
+    (AsyncStorage.removeItem as jest.Mock).mockRejectedValue(mockError);
+
+    await StorageAdapter.removeItem(mockKey);
+
+    expect(AsyncStorage.removeItem).toHaveBeenCalledWith(mockKey);
+    expect(Alert.alert).toHaveBeenCalledWith("Error", "Failed to delete data");
+  });
+
+  test("Debe manejar errores al eliminar múltiples elementos de AsyncStorage", async () => {
+    (AsyncStorage.multiRemove as jest.Mock).mockRejectedValue(mockError);
+
+    await StorageAdapter.multiRemove(mockKeys);
+
+    expect(AsyncStorage.multiRemove).toHaveBeenCalledWith(mockKeys);
+    expect(Alert.alert).toHaveBeenCalledWith(
+      "Error",
+      "Failed to delete data specified"
+    );
+  });
+
+   test("Debe manejar errores al obtener todas las claves de AsyncStorage", async () => {
+     (AsyncStorage.getAllKeys as jest.Mock).mockRejectedValue(mockError);
+
+     const result = await StorageAdapter.getAllKeys();
+
+     expect(AsyncStorage.getAllKeys).toHaveBeenCalled();
+     expect(Alert.alert).toHaveBeenCalledWith(
+       "Error",
+       "Failed to get all keys"
+     );
+     expect(result).toEqual([]);
+   });
+  
+  test("Debe manejar errores al limpiar AsyncStorage", async () => {
+    (AsyncStorage.clear as jest.Mock).mockRejectedValue(mockError);
+
+    await StorageAdapter.clear();
+
+    expect(AsyncStorage.clear).toHaveBeenCalled();
+    expect(Alert.alert).toHaveBeenCalledWith("Error", "Failed to clear data");
+  });
+
 });
--- presentation/shared/store/index.ts
+++ presentation/shared/store/index.ts
@@ -1,2 +1,3 @@
+export * from './useCameraStore';
 export * from './useLocationPermissionsStore';
 export * from './useLocationStore';
